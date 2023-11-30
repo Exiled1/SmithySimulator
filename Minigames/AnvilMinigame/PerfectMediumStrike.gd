1521f6a03f9bafe68a76b235cc352a9b753f3cd6
Exiled1 <rudyperalta831@gmail.com>
2023-11-29 22:33:39 -0800

    Added prototype for anvil minigame, I hate Sprites.
    Im gonna kms, sprites didn't need to be this depressing.

--- Minigames/AnvilMinigame/PerfectMediumStrike.gd
+++ Minigames/AnvilMinigame/PerfectMediumStrike.gd
@@ -10,7 +10,7 @@ func _ready():
 	var bg_texture: Texture2D = %Background.texture
 	height_of_bar = bg_texture.get_height()
 	width_of_bar = bg_texture.get_width()
-	set_minigame_difficulty(.1, .4, Vector2(height_of_bar, width_of_bar))
+	set_minigame_difficulty(.1, .6, Vector2(height_of_bar, width_of_bar))
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,13 +26,20 @@ func set_minigame_difficulty(green_percent: float, orange_percent: float = 1, ba
 	# Green bar. Set the % to what we passed in.
 	var perf_hit_sprite: Sprite2D = %PerfectHit
 	perf_hit_sprite.scale.x = green_percent
+	# Cuz the fucking texture is the actual size of the image. Fml.
+	var perf_hit_width = perf_hit_sprite.texture.get_width() * green_percent * %PerfectHitContainer.scale.x 
 	$PerfectHitContainer.position = Vector2(0,0) # Center the green rect on our container.
 	
 	# Left Orange bar.
 	var medium_left_sprite: Sprite2D = %MediumHitLeft
-	medium_left_sprite.scale.x = orange_percent / 2.0 # Half b/c the passed percent is the whole %
+	medium_left_sprite.scale.x = orange_percent / 2.0 # Half b/c the passed percent is the whole %\
+	# Bro this is cursed, the texture size doesn't scale. And the orange percent is double what it should be. Fml.
+	var orange_width = medium_left_sprite.texture.get_width() * (orange_percent * .5) * $MediumLeftContainer.scale.x
+	# Position of the orange bar should be 1/2 green_width + 1/2 orange_width
+	%MediumLeftContainer.position = Vector2((orange_width + perf_hit_width) * .5, 0)
 	
 	# Right Orange bar
 	var medium_right_sprite: Sprite2D = %MediumHitRight
 	medium_right_sprite.scale.x = orange_percent / 2.0 # Half b/c the passed percent is the whole %
-	
+	# Same shit but negative.
+	%MediumRightContainer.position = Vector2(-(orange_width + perf_hit_width) * .5, 0)
